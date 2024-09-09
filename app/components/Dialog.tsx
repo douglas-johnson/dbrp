13c545e7db52441e15fa80f19778b7acf150aa32
Douglas Johnson <doug@thoughtcatalog.com>
2024-09-09 14:43:59 -0400

    move close button to the top

--- app/components/Dialog.tsx
+++ app/components/Dialog.tsx
@@ -6,8 +6,8 @@ function Dialog( props, ref ) {
 
 	return (
 		<dialog className="dbrp-dialog" ref={ref}>
-			{props.children}
 			<button type="button" onClick={() => ref.current?.close()}> Close </button>
+			{props.children}
 		</dialog>
 	)
 
