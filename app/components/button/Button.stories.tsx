0e38f75143ac04831da0641ae32a0dc705bb8e61
Douglas Johnson <doug@thoughtcatalog.com>
2026-09-01 08:18:46 -0400

    Add stories for button states, product options

--- app/components/button/Button.stories.tsx
+++ app/components/button/Button.stories.tsx
@@ -27,9 +27,60 @@ export const Default: Story = {
   },
 };
 
+export const Primary: Story = {
+  args: {
+    className: 'button is-primary',
+    children: 'Hello Button',
+  },
+};
+
 export const Transparent: Story = {
   args: {
     className: 'button is-transparent',
     children: 'Hello Transparent Button',
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    className: 'button is-transparent is-icon',
+    children: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path d="M2.4,24l-2.4-2.4,9.6-9.6L0,2.4,2.4,0l9.6,9.6L21.6,0l2.4,2.4-9.6,9.6,9.6,9.6-2.4,2.4-9.6-9.6L2.4,24Z" />
+      </svg>
+    ),
+  },
+};
+
+export const ProductOption: Story = {
+  args: {
+    className: 'button product-options-item',
+    children: 'SM',
+  },
+};
+
+export const ProductOptionLink: Story = {
+  args: {
+    className: 'button product-options-item is-selectable',
+    children: 'SM',
+  },
+};
+
+export const ProductOptionSelected: Story = {
+  args: {
+    className: 'button product-options-item is-selected',
+    children: 'SM',
+  },
+};
+
+export const ProductOptionUnavailable: Story = {
+  args: {
+    className: 'button product-options-item is-unavailable',
+    children: 'SM',
   },
 };
