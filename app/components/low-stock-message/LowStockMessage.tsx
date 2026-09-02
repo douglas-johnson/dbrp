7b4f556e19e54189052d00da233b13e4613cddba
Douglas Johnson <doug@thoughtcatalog.com>
2026-09-02 09:13:44 -0400

    add low stock message component

---
+++ app/components/low-stock-message/LowStockMessage.tsx
@@ -0,0 +1,24 @@
+import {useRootLoaderData} from '~/lib/root-data';
+import type {Maybe} from '@shopify/hydrogen/storefront-api-types';
+
+export const DEFAULT_LOW_STOCK_THRESHOLD = 4;
+
+export default function LowStockMessage({
+  quantityAvailable,
+}: {
+  quantityAvailable: Maybe<number> | undefined;
+}) {
+  const {header} = useRootLoaderData();
+  const lowStockThreshold = Number(
+    header.shop.lowStockThreshold?.value ?? DEFAULT_LOW_STOCK_THRESHOLD,
+  );
+  if ('number' !== typeof quantityAvailable || 0 >= quantityAvailable) {
+    return null;
+  }
+  if (lowStockThreshold < quantityAvailable) {
+    return null;
+  }
+  return (
+    <p className="low-stock-message">Only {quantityAvailable} left in stock.</p>
+  );
+}
