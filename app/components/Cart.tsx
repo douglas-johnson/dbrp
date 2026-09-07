cbda018538c6a48116e4d80d8cc911f1cad43254
Douglas Johnson <226381+douglas-johnson@users.noreply.github.com>
2026-09-07 07:04:50 -0400

    Link to collections/all in the cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -311,10 +311,10 @@ export function CartEmpty({
       </p>
       <br />
       <Link
-        to="/collections"
+        to="/collections/all"
         onClick={() => {
           if (layout === 'aside') {
-            window.location.href = '/collections';
+            window.location.href = '/collections/all';
           }
         }}
       >
